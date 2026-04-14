fd5eff84976695693a7ef6bf0808fd8e192b1510
Ishan Handa <ishanhanda7733@gmail.com>
2026-04-14 15:43:09 +0530

    feat: add custom user model with email authentication and roles

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 # Create your models here.
-class UserManger(BaseUserManager):
+class UserManager(BaseUserManager):
     def create_user(self, email, password=None, role='PASSENGER'):
         if not email:
             raise ValueError("Users must have an email!")
+        email = self.normalize_email(email)
+        user = self.model(email=email, role=role)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self,email,password):
+        user = self.create_user(email,password, role='ADMIN')
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICES = (
+        ('PASSENGER', 'Passenger'),
+        ('DRIVER', 'Driver'),
+        ('ADMIN', 'Admin'),
+    )
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15, blank=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    def __str__(self):
+        return self.email
